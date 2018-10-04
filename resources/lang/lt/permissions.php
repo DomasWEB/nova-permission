7b1cc571b8f79d7de6b07757c276bcdc57fee2f3
Dominykas Tijūnaitis <dominykas@tijunaitis.lt>
2018-10-04 21:18:32 +0300

    Create permissions.php

---
+++ resources/lang/lt/permissions.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'name' => 'Pavadinimas',
+    'display_name' => 'Atvaizduojamas pavadinimas',
+    'guard_name' => 'Apsaugos pavadinimas',
+    'created_at' => 'Sukurta',
+    'updated_at' => 'Atnaujinta',
+];
